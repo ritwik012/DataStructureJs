e311cda8f0380fb3818ca4725e6b9d514f012ec1
“ritwik012” <rutwikgulhaneg@gmail.com>
2022-01-22 23:16:05 +0530

    Added Sorted Second Largest & Second Smallest Element File

---
+++ SortedSecLarSecSmall.js
@@ -0,0 +1,22 @@
+var randoms = [...Array(10)].map(() => Math.floor(Math.random() * 1000));
+var sortnum=randoms.sort(); //sort the numbers
+console.log(sortnum);
+
+const minimumIndex = randoms => {
+   return randoms.indexOf(Math.min(...randoms));  
+};
+const maximumIndex = randoms => {
+    return randoms.indexOf(Math.max(...randoms));
+ };
+const secondMinimum = randoms => {
+    const copy = randoms.slice();
+    copy.splice(minimumIndex(copy), 1);
+    return copy[minimumIndex(copy)];
+ };
+ console.log("Second Smallets Number:- "+secondMinimum(randoms));
+ const secondMax = randoms => {
+    const copy = randoms.slice();
+    copy.splice(maximumIndex(copy), 1);
+    return copy[maximumIndex(copy)];
+ };
+ console.log("Second Largest Number:- "+secondMax(randoms));
